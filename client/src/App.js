e8180ce7196e422ff4db2bafe9ab19d80067cc95
Sprihanjay <banik.sprihanjay@gmail.com>
2024-08-03 13:45:03 -0400

    Made the frontpage with reusable elements

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Form from './modules/form';
+
+
+function App() {
+  return (
+    <div className='bg-[#edf3fc] h-screen flex justify-center items-center'>
+      <Form/>
+    </div>
+  );
+}
+
+export default App;
